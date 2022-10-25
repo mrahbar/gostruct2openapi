3ec67efc0f074e4de2e6b96a99b6c5af92e4c02d
Mahmoud Rahbar Azad <mahmoudreza.rahbarazad@ottogroup.com>
2022-10-25 22:00:44 +0200

    introduce models

--- doc/generate.go
+++ doc/generate.go
@@ -50,8 +50,13 @@ func (o *openapiGenerator) parse(pkgs []*packages.Package, filter *regexp.Regexp
 			if obj != nil && obj.Type() != nil && obj.Type().Underlying() != nil {
 				if _struct, ok := obj.Type().Underlying().(*types.Struct); ok { //skip if underlying scope is not a struct, e.g. interface
 					fmt.Printf("Processing struct: name=%s\n", structScopeName)
-					props := spec.SchemaProps{ID: structScopeName, Type: []string{objectType}, Properties: make(spec.SchemaProperties)}
-					subSpecs := o.toSpec(&props, _struct, commentMap, structScopeName)
+					var props = spec.SchemaProps{ID: structScopeName, Type: []string{objectType}, Properties: make(spec.SchemaProperties)}
+					target := &targetStruct{
+						name:       structScopeName,
+						origType:   obj.Type(),
+						origStruct: _struct,
+					}
+					subSpecs := o.toSpec(&props, target, commentMap)
 					specs = append(specs, spec.Schema{SchemaProps: props})
 					for _, s := range subSpecs {
 						specs = append(specs, s)
@@ -64,12 +69,17 @@ func (o *openapiGenerator) parse(pkgs []*packages.Package, filter *regexp.Regexp
 							if !filter.MatchString(methodScopeName) {
 								continue
 							}
-							obj := scope.Lookup(methodScopeName) //TODO try to rename ID of methodScopeName to public name using @title
+							obj := scope.Lookup(methodScopeName)
 							if obj != nil && obj.Type() != nil && obj.Type().Underlying() != nil {
 								if _struct, ok := obj.Type().Underlying().(*types.Struct); ok { //skip if underlying scope is not a struct, e.g. interface
 									fmt.Printf("Processing method struct: name=%s\n", methodScopeName)
-									props := spec.SchemaProps{ID: methodScopeName, Type: []string{objectType}, Properties: make(spec.SchemaProperties)}
-									subSpecs := o.toSpec(&props, _struct, commentMap, methodScopeName)
+									var props = spec.SchemaProps{ID: methodScopeName, Type: []string{objectType}, Properties: make(spec.SchemaProperties)}
+									target := &targetStruct{
+										name:       methodScopeName,
+										origType:   obj.Type(),
+										origStruct: _struct,
+									}
+									subSpecs := o.toSpec(&props, target, commentMap)
 									specs = append(specs, spec.Schema{SchemaProps: props})
 									for _, s := range subSpecs {
 										specs = append(specs, s)
@@ -87,15 +97,35 @@ func (o *openapiGenerator) parse(pkgs []*packages.Package, filter *regexp.Regexp
 	return
 }
 
-func (o *openapiGenerator) toSpec(props *spec.SchemaProps, _struct *types.Struct, commentMap map[string]string, structName string) map[string]spec.Schema {
+type targetStruct struct {
+	name       string
+	origType   types.Type
+	origStruct *types.Struct
+}
+
+type targetField struct {
+	structName  string
+	fieldTag    string
+	fieldName   string
+	specField   specField
+	elem        types.Type
+	isArrayType bool
+}
+
+func (o *openapiGenerator) toSpec(props *spec.SchemaProps, target *targetStruct, commentMap map[string]string) map[string]spec.Schema {
 	specs := make(map[string]spec.Schema)
 
-	for i := 0; i < _struct.NumFields(); i++ {
-		field := _struct.Field(i)
+	for i := 0; i < target.origStruct.NumFields(); i++ {
+		field := target.origStruct.Field(i)
 
 		if field.Embedded() {
 			if _embeddedStruct, ok := field.Type().Underlying().(*types.Struct); ok {
-				subSpecs := o.toSpec(props, _embeddedStruct, commentMap, field.Name())
+				embeddedTarget := &targetStruct{
+					name:       field.Name(),
+					origType:   field.Type(),
+					origStruct: _embeddedStruct,
+				}
+				subSpecs := o.toSpec(props, embeddedTarget, commentMap)
 				for k, s := range subSpecs {
 					specs[k] = s
 				}
@@ -106,35 +136,79 @@ func (o *openapiGenerator) toSpec(props *spec.SchemaProps, _struct *types.Struct
 
 			//early handling of time.Time due to underlying type is actually a struct
 			if fieldType := structFieldTypeMap[underlying.String()]; fieldType.format == "RFC3339" {
-				o.mapField(props, _struct.Tag(i), commentMap, structName, fieldName, fieldType)
+				tf := &targetField{
+					structName: target.name,
+					fieldTag:   target.origStruct.Tag(i),
+					fieldName:  fieldName,
+					specField:  fieldType,
+				}
+				o.mapField(props, tf, commentMap)
 				continue
 			}
 
 			switch u := underlying.(type) {
 			case *types.Interface:
 				// falling back to object type because handling of interface type is not possible
-				o.mapField(props, _struct.Tag(i), commentMap, structName, fieldName, specField{baseType: objectType})
+				tf := &targetField{
+					structName: target.name,
+					fieldTag:   target.origStruct.Tag(i),
+					fieldName:  fieldName,
+					specField:  specField{baseType: objectType},
+				}
+				o.mapField(props, tf, commentMap)
 			case *types.Struct:
-				name := field.Type().(*types.Named).Obj().Name()
-				o.mapField(props, _struct.Tag(i), commentMap, structName, fieldName, specField{ref: name})
+				obj := field.Type().(*types.Named).Obj()
+				name := obj.Name()
+				tf := &targetField{
+					structName: target.name,
+					fieldTag:   target.origStruct.Tag(i),
+					fieldName:  fieldName,
+					specField:  specField{ref: name},
+				}
+				o.mapField(props, tf, commentMap)
+
 				subProps := spec.SchemaProps{ID: name, Type: []string{objectType}, Properties: make(spec.SchemaProperties)}
-				subSpecs := o.toSpec(&subProps, u, commentMap, name)
+				subTarget := &targetStruct{
+					name:       name,
+					origType:   field.Type(),
+					origStruct: u,
+				}
+				subSpecs := o.toSpec(&subProps, subTarget, commentMap)
 				specs[name] = spec.Schema{SchemaProps: subProps}
 				for k, s := range subSpecs {
 					specs[k] = s
 				}
 			case *types.Pointer:
-				subSpecs := o.handleUnderlyingField(props, commentMap, structName, fieldName, _struct.Tag(i), u.Elem(), false)
+				tf := &targetField{
+					structName: target.name,
+					fieldTag:   target.origStruct.Tag(i),
+					fieldName:  fieldName,
+					elem:       u.Elem(),
+				}
+				subSpecs := o.handleUnderlyingField(props, tf, commentMap)
 				for k, s := range subSpecs {
 					specs[k] = s
 				}
 			case *types.Slice:
-				subSpecs := o.handleUnderlyingField(props, commentMap, structName, fieldName, _struct.Tag(i), u.Elem(), true)
+				tf := &targetField{
+					structName:  target.name,
+					fieldTag:    target.origStruct.Tag(i),
+					fieldName:   fieldName,
+					elem:        u.Elem(),
+					isArrayType: true,
+				}
+				subSpecs := o.handleUnderlyingField(props, tf, commentMap)
 				for k, s := range subSpecs {
 					specs[k] = s
 				}
 			default:
-				o.mapField(props, _struct.Tag(i), commentMap, structName, fieldName, structFieldTypeMap[underlying.String()])
+				tf := &targetField{
+					structName: target.name,
+					fieldTag:   target.origStruct.Tag(i),
+					fieldName:  fieldName,
+					specField:  structFieldTypeMap[underlying.String()],
+				}
+				o.mapField(props, tf, commentMap)
 			}
 		}
 	}
@@ -142,52 +216,64 @@ func (o *openapiGenerator) toSpec(props *spec.SchemaProps, _struct *types.Struct
 	return specs
 }
 
-func (o *openapiGenerator) handleUnderlyingField(props *spec.SchemaProps, commentMap map[string]string, structName string,
-	fieldName string, tag string, elem types.Type, isArrayType bool) map[string]spec.Schema {
+//, structName string, fieldName string, tag string, elem types.Type, isArrayType bool
+func (o *openapiGenerator) handleUnderlyingField(props *spec.SchemaProps, target *targetField, commentMap map[string]string) map[string]spec.Schema {
 	specs := make(map[string]spec.Schema)
 
-	if _underlyingStruct, ok := elem.Underlying().(*types.Pointer); ok {
-		return o.handleUnderlyingField(props, commentMap, structName, fieldName, tag, _underlyingStruct.Elem(), isArrayType)
-	} else if _underlyingStruct, ok := elem.Underlying().(*types.Struct); ok {
-		name := elem.(*types.Named).Obj().Name()
+	if _underlyingStruct, ok := target.elem.Underlying().(*types.Pointer); ok {
+		target.elem = _underlyingStruct.Elem()
+		return o.handleUnderlyingField(props, target, commentMap)
+	} else if _underlyingStruct, ok := target.elem.Underlying().(*types.Struct); ok {
+		field := target.elem.(*types.Named).Obj()
+		name := field.Name()
 		_specField := specField{ref: name}
-		if isArrayType {
+		if target.isArrayType {
 			_specField.baseType = arrayType
 		}
-		o.mapField(props, tag, commentMap, structName, fieldName, _specField)
+		target.specField = _specField
+		o.mapField(props, target, commentMap)
+
 		subProps := spec.SchemaProps{ID: name, Type: []string{objectType}, Properties: make(spec.SchemaProperties)}
-		subSpecs := o.toSpec(&subProps, _underlyingStruct, commentMap, name)
+		subTarget := &targetStruct{
+			name:       name,
+			origType:   field.Type(),
+			origStruct: _underlyingStruct,
+		}
+		subSpecs := o.toSpec(&subProps, subTarget, commentMap)
 		specs[name] = spec.Schema{SchemaProps: subProps}
 		for k, s := range subSpecs {
 			specs[k] = s
 		}
-	} else if _underlyingBasic, ok := elem.Underlying().(*types.Basic); ok {
+	} else if _underlyingBasic, ok := target.elem.Underlying().(*types.Basic); ok {
 		var _specField specField
-		if isArrayType {
+		if target.isArrayType {
 			_specField = specField{baseType: arrayType, itemsType: structFieldTypeMap[_underlyingBasic.String()].baseType}
 		} else {
 			_specField = structFieldTypeMap[_underlyingBasic.String()]
 		}
-		o.mapField(props, tag, commentMap, structName, fieldName, _specField)
+		target.specField = _specField
+		o.mapField(props, target, commentMap)
 	} else {
-		fmt.Printf("has no basic type but %s", elem.Underlying().String())
+		fmt.Printf("has no basic type but %s", target.elem.Underlying().String())
 		var _specField specField
-		if isArrayType {
+		if target.isArrayType {
 			_specField = specField{baseType: arrayType, itemsType: objectType}
 		} else {
 			_specField = specField{baseType: objectType}
 		}
-		o.mapField(props, tag, commentMap, structName, fieldName, _specField)
+		target.specField = _specField
+		o.mapField(props, target, commentMap)
 	}
 
 	return specs
 }
 
-func (o *openapiGenerator) mapField(props *spec.SchemaProps, tag string, commentMap map[string]string, structName, fieldName string, specField specField) {
-	description := strings.Replace(commentMap[fmt.Sprintf("%s.%s", structName, fieldName)], "\n", "", -1)
+func (o *openapiGenerator) mapField(props *spec.SchemaProps, target *targetField, commentMap map[string]string) {
+	description := strings.Replace(commentMap[fmt.Sprintf("%s.%s", target.structName, target.fieldName)], "\n", "", -1)
 
-	if len(tag) > 0 {
-		tags, err := structtag.Parse(tag)
+	var fieldName = target.fieldName
+	if len(target.fieldTag) > 0 {
+		tags, err := structtag.Parse(target.fieldTag)
 		if err == nil {
 			for _, tp := range tags.Tags() {
 				if tp.Key == "json" {
@@ -198,24 +284,24 @@ func (o *openapiGenerator) mapField(props *spec.SchemaProps, tag string, comment
 	}
 
 	schemaProps := spec.SchemaProps{
-		Format:      specField.format,
+		Format:      target.specField.format,
 		Description: description,
 	}
 
-	if specField.baseType == arrayType {
+	if target.specField.baseType == arrayType {
 		var props spec.SchemaProps
-		if specField.ref != "" {
-			props = spec.SchemaProps{Ref: spec.MustCreateRef("#/components/schemas/" + specField.ref)}
-		} else if specField.itemsType != "" {
-			props = spec.SchemaProps{Type: []string{specField.itemsType}}
+		if target.specField.ref != "" {
+			props = spec.SchemaProps{Ref: spec.MustCreateRef("#/components/schemas/" + target.specField.ref)}
+		} else if target.specField.itemsType != "" {
+			props = spec.SchemaProps{Type: []string{target.specField.itemsType}}
 		}
-		schemaProps.Type = []string{specField.baseType}
+		schemaProps.Type = []string{target.specField.baseType}
 		schemaProps.Items = &spec.SchemaOrArray{Schema: &spec.Schema{SchemaProps: props}}
 	} else {
-		if specField.ref != "" {
-			schemaProps.Ref = spec.MustCreateRef("#/components/schemas/" + specField.ref)
+		if target.specField.ref != "" {
+			schemaProps.Ref = spec.MustCreateRef("#/components/schemas/" + target.specField.ref)
 		} else {
-			schemaProps.Type = []string{specField.baseType}
+			schemaProps.Type = []string{target.specField.baseType}
 		}
 	}
 
